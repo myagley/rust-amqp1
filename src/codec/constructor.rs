233a9506a730f9ce522e23e545596820bb181dcd
Mike Yagley <myagley@gmail.com>
2017-08-23 19:28:10 -0700

    Support dynamic size for constructed values

--- src/codec/constructor.rs
+++ src/codec/constructor.rs
@@ -9,9 +9,33 @@ use uuid::Uuid;
 
 pub struct Constructor<T> {
     format_code: u8,
-    size: usize,
+    size: fn(&T) -> usize,
     decode: fn(&[u8]) -> IResult<&[u8], T, u32>,
     encode: fn(&T, &mut BytesMut) -> (),
+}
+
+fn size0<T>(_: &T) -> usize {
+    0
+}
+
+fn size1<T>(_: &T) -> usize {
+    1
+}
+
+fn size2<T>(_: &T) -> usize {
+   2 
+}
+
+fn size4<T>(_: &T) -> usize {
+   4 
+}
+
+fn size8<T>(_: &T) -> usize {
+   8 
+}
+
+fn size16<T>(_: &T) -> usize {
+  16 
 }
 
 impl<T> Clone for Constructor<T> {
@@ -42,10 +66,15 @@ impl<T> Constructor<T> {
     }
 
     pub fn encode_value(&self, t: &T, bytes: &mut BytesMut) {
-        if bytes.remaining_mut() < self.size {
-            bytes.reserve(self.size);
+        let size = self.encoded_value_size(t);
+        if bytes.remaining_mut() < size {
+            bytes.reserve(size);
         }
         (self.encode)(t, bytes)
+    }
+
+    pub fn encoded_value_size(&self, t: &T) -> usize {
+        (self.size)(t)
     }
 }
 
@@ -54,14 +83,14 @@ impl Constructor<Null> {
         IResult::Done(bytes, Null)
     }
 
-    fn encode_null(null: &Null, bytes: &mut BytesMut) { }
+    fn encode_null(_: &Null, _: &mut BytesMut) { }
 }
 
 impl Decode for Constructor<Null> {
     named!(decode<Self>, map!(tag!([0x40u8]), |_|
         Constructor {
             format_code: 0x40u8,
-            size: 0,
+            size: size0::<Null>,
             decode: Constructor::<Null>::decode_null,
             encode: Constructor::<Null>::encode_null,
         }
@@ -83,7 +112,7 @@ impl Constructor<bool> {
         IResult::Done(bytes, true)
     }
 
-    fn encode_fixed0(b: &bool, bytes: &mut BytesMut) { }
+    fn encode_fixed0(_: &bool, _: &mut BytesMut) { }
 
     fn encode_fixed1(b: &bool, bytes: &mut BytesMut) {
         if *b { bytes.put_u8(0x01u8) } else { bytes.put_u8(0x00u8) }
@@ -95,7 +124,7 @@ impl Decode for Constructor<bool> {
         map!(tag!([0x56u8]), |_|
             Constructor {
                 format_code: 0x56u8,
-                size: 1,
+                size: size1::<bool>,
                 decode: Constructor::<bool>::decode_fixed1,
                 encode: Constructor::<bool>::encode_fixed1,
             }
@@ -103,7 +132,7 @@ impl Decode for Constructor<bool> {
         map!(tag!([0x41u8]), |_|
             Constructor {
                 format_code: 0x41u8,
-                size: 0,
+                size: size0::<bool>,
                 decode: Constructor::<bool>::decode_true_fixed0,
                 encode: Constructor::<bool>::encode_fixed0,
             }
@@ -111,7 +140,7 @@ impl Decode for Constructor<bool> {
         map!(tag!([0x42u8]), |_|
             Constructor {
                 format_code: 0x42u8,
-                size: 0,
+                size: size0::<bool>,
                 decode: Constructor::<bool>::decode_false_fixed0,
                 encode: Constructor::<bool>::encode_fixed0,
             }
@@ -129,7 +158,7 @@ impl Decode for Constructor<u8> {
     named!(decode<Constructor<u8>>, map!(tag!([0x50u8]), |_| 
         Constructor {
             format_code: 0x50u8,
-            size: 1,
+            size: size1::<u8>,
             decode: be_u8,
             encode: Constructor::<u8>::encode_fixed,
         }
@@ -146,7 +175,7 @@ impl Decode for Constructor<u16> {
     named!(decode<Constructor<u16>>, map!(tag!([0x60u8]), |_|
         Constructor {
             format_code: 0x60u8,
-            size: 2,
+            size: size2::<u16>,
             decode: be_u16,
             encode: Constructor::<u16>::encode_fixed,
         }
@@ -161,7 +190,7 @@ impl Constructor<u32> {
         IResult::Done(bytes, 0)
     }
 
-    fn encode_zero(b: &u32, bytes: &mut BytesMut) { }
+    fn encode_zero(_: &u32, _: &mut BytesMut) { }
 
     fn encode_small(b: &u32, bytes: &mut BytesMut) {
         bytes.put_u8(*b as u8)
@@ -177,7 +206,7 @@ impl Decode for Constructor<u32> {
         map!(tag!([0x70u8]), |_|
             Constructor {
                 format_code: 0x70u8,
-                size: 4,
+                size: size4::<u32>,
                 decode: be_u32,
                 encode: Constructor::<u32>::encode_large,
             }
@@ -185,7 +214,7 @@ impl Decode for Constructor<u32> {
         map!(tag!([0x52u8]), |_|
             Constructor {
                 format_code: 0x52u8,
-                size: 1,
+                size: size1::<u32>,
                 decode: Constructor::<u32>::decode_small,
                 encode: Constructor::<u32>::encode_small,
             }
@@ -193,7 +222,7 @@ impl Decode for Constructor<u32> {
         map!(tag!([0x43u8]), |_|
             Constructor {
                 format_code: 0x43u8,
-                size: 0,
+                size: size0::<u32>,
                 decode: Constructor::<u32>::decode_zero,
                 encode: Constructor::<u32>::encode_zero,
             }
@@ -209,7 +238,7 @@ impl Constructor<u64> {
         IResult::Done(bytes, 0)
     }
 
-    fn encode_zero(b: &u64, bytes: &mut BytesMut) { }
+    fn encode_zero(_: &u64, _: &mut BytesMut) { }
 
     fn encode_small(b: &u64, bytes: &mut BytesMut) {
         bytes.put_u8(*b as u8)
@@ -225,7 +254,7 @@ impl Decode for Constructor<u64> {
         map!(tag!([0x80u8]), |_|
             Constructor {
                 format_code: 0x80u8,
-                size: 8,
+                size: size8::<u64>,
                 decode: be_u64,
                 encode: Constructor::<u64>::encode_large,
             }
@@ -233,7 +262,7 @@ impl Decode for Constructor<u64> {
         map!(tag!([0x53u8]), |_|
             Constructor {
                 format_code: 0x53u8,
-                size: 1,
+                size: size1::<u64>,
                 decode: Constructor::<u64>::decode_small,
                 encode: Constructor::<u64>::encode_small,
             }
@@ -241,7 +270,7 @@ impl Decode for Constructor<u64> {
         map!(tag!([0x44u8]), |_|
             Constructor {
                 format_code: 0x44u8,
-                size: 0,
+                size: size0::<u64>,
                 decode: Constructor::<u64>::decode_zero,
                 encode: Constructor::<u64>::encode_zero,
             })
@@ -258,7 +287,7 @@ impl Decode for Constructor<i8> {
     named!(decode<Constructor<i8>>, map!(tag!([0x51u8]), |_| 
         Constructor {
             format_code: 0x51u8,
-            size: 1,
+            size: size1::<i8>,
             decode: be_i8,
             encode: Constructor::<i8>::encode_fixed,
         }
@@ -275,7 +304,7 @@ impl Decode for Constructor<i16> {
     named!(decode<Constructor<i16>>, map!(tag!([0x61u8]), |_|
         Constructor {
             format_code: 0x61u8,
-            size: 2,
+            size: size2::<i16>,
             decode: be_i16,
             encode: Constructor::<i16>::encode_fixed,
         }
@@ -300,7 +329,7 @@ impl Decode for Constructor<i32> {
         map!(tag!([0x71u8]), |_|
             Constructor {
                 format_code: 0x71u8,
-                size: 4,
+                size: size4::<i32>,
                 decode: be_i32,
                 encode: Constructor::<i32>::encode_large,
             }
@@ -308,7 +337,7 @@ impl Decode for Constructor<i32> {
         map!(tag!([0x54u8]), |_|
             Constructor {
                 format_code: 0x54u8,
-                size: 1,
+                size: size1::<i32>,
                 decode: Constructor::<i32>::decode_small,
                 encode: Constructor::<i32>::encode_small,
             }
@@ -334,7 +363,7 @@ impl Decode for Constructor<i64> {
         map!(tag!([0x81u8]), |_|
             Constructor {
                 format_code: 0x81u8,
-                size: 8,
+                size: size8::<i64>,
                 decode: be_i64,
                 encode: Constructor::<i64>::encode_large,
             }
@@ -342,7 +371,7 @@ impl Decode for Constructor<i64> {
         map!(tag!([0x55u8]), |_|
             Constructor {
                 format_code: 0x55u8,
-                size: 1,
+                size: size1::<i64>,
                 decode: Constructor::<i64>::decode_small,
                 encode: Constructor::<i64>::encode_small,
             }
@@ -360,7 +389,7 @@ impl Decode for Constructor<f32> {
     named!(decode<Constructor<f32>>, map!(tag!([0x72u8]), |_| 
         Constructor {
             format_code: 0x72,
-            size: 4,
+            size: size4::<f32>,
             decode: be_f32,
             encode: Constructor::<f32>::encode_float,
         }
@@ -377,7 +406,7 @@ impl Decode for Constructor<f64> {
     named!(decode<Constructor<f64>>, map!(tag!([0x82u8]), |_|
         Constructor {
             format_code: 0x82,
-            size: 8,
+            size: size8::<f64>,
             decode: be_f64,
             encode: Constructor::<f64>::encode_double,
         }
@@ -397,7 +426,7 @@ impl Decode for Constructor<char> {
     named!(decode<Constructor<char>>, map!(tag!([0x73u8]), |_|
         Constructor {
             format_code: 0x73,
-            size: 4,
+            size: size4::<char>,
             decode: Constructor::<char>::decode_u32,
             encode: Constructor::<char>::encode_u32,
         }
@@ -418,7 +447,7 @@ impl Decode for Constructor<DateTime<Utc>> {
     named!(decode<Constructor<DateTime<Utc>>>, map!(tag!([0x83u8]), |_|
         Constructor {
             format_code: 0x83,
-            size: 8,
+            size: size8::<DateTime<Utc>>,
             decode: Constructor::<DateTime<Utc>>::decode_millis,
             encode: Constructor::<DateTime<Utc>>::encode_millis,
         }
@@ -437,7 +466,7 @@ impl Decode for Constructor<Uuid> {
     named!(decode<Constructor<Uuid>>, map!(tag!([0x98u8]), |_|
         Constructor {
             format_code: 0x98,
-            size: 16,
+            size: size16::<Uuid>,
             decode: Constructor::<Uuid>::decode_bytes,
             encode: Constructor::<Uuid>::encode_bytes,
         }
@@ -458,6 +487,14 @@ impl Constructor<Bytes> {
         bytes.put_u32::<BigEndian>(b.len() as u32);
         bytes.put(b);
     }
+
+    fn size_short(b: &Bytes) -> usize {
+        1 + b.len()
+    }
+
+    fn size_long(b: &Bytes) -> usize {
+        4 + b.len()
+    }
 }
 
 impl Decode for Constructor<Bytes> {
@@ -465,7 +502,7 @@ impl Decode for Constructor<Bytes> {
         map!(tag!([0xA0u8]), |_|
             Constructor {
                 format_code: 0xA0,
-                size: 0,
+                size: Constructor::<Bytes>::size_short,
                 decode: Constructor::<Bytes>::decode_short,
                 encode: Constructor::<Bytes>::encode_short,
             }
@@ -473,7 +510,7 @@ impl Decode for Constructor<Bytes> {
         map!(tag!([0xB0u8]), |_|
             Constructor {
                 format_code: 0xB0,
-                size: 0,
+                size: Constructor::<Bytes>::size_long,
                 decode: Constructor::<Bytes>::decode_long,
                 encode: Constructor::<Bytes>::encode_long,
             }
@@ -495,6 +532,14 @@ impl Constructor<ByteStr> {
         bytes.put_u32::<BigEndian>(b.len() as u32);
         bytes.put(b.as_bytes());
     }
+
+    fn size_short(b: &ByteStr) -> usize {
+        1 + b.len()
+    }
+
+    fn size_long(b: &ByteStr) -> usize {
+        4 + b.len()
+    }
 }
 
 impl Decode for Constructor<ByteStr> {
@@ -502,7 +547,7 @@ impl Decode for Constructor<ByteStr> {
         map!(tag!([0xA1u8]), |_|
             Constructor {
                 format_code: 0xA1,
-                size: 0,
+                size: Constructor::<ByteStr>::size_short,
                 decode: Constructor::<ByteStr>::decode_short,
                 encode: Constructor::<ByteStr>::encode_short,
             }
@@ -510,7 +555,7 @@ impl Decode for Constructor<ByteStr> {
         map!(tag!([0xB1u8]), |_|
             Constructor {
                 format_code: 0xB1,
-                size: 0,
+                size: Constructor::<ByteStr>::size_long,
                 decode: Constructor::<ByteStr>::decode_long,
                 encode: Constructor::<ByteStr>::encode_long,
             }
@@ -532,6 +577,14 @@ impl Constructor<Symbol> {
         bytes.put_u32::<BigEndian>(b.len() as u32);
         bytes.put(b.as_bytes());
     }
+
+    fn size_short(b: &Symbol) -> usize {
+        1 + b.len()
+    }
+
+    fn size_long(b: &Symbol) -> usize {
+        4 + b.len()
+    }
 }
 
 impl Decode for Constructor<Symbol> {
@@ -539,7 +592,7 @@ impl Decode for Constructor<Symbol> {
         map!(tag!([0xA3u8]), |_|
             Constructor {
                 format_code: 0xA3,
-                size: 0,
+                size: Constructor::<Symbol>::size_short,
                 decode: Constructor::<Symbol>::decode_short,
                 encode: Constructor::<Symbol>::encode_short,
             }
@@ -547,7 +600,7 @@ impl Decode for Constructor<Symbol> {
         map!(tag!([0xB3u8]), |_|
             Constructor {
                 format_code: 0xB3,
-                size: 0,
+                size: Constructor::<Symbol>::size_long,
                 decode: Constructor::<Symbol>::decode_long,
                 encode: Constructor::<Symbol>::encode_long,
             }
